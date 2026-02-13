7df4ac956f63a4f89ebc4182c73468d4b12bcf8c
Rounak <rounak@Rounaks-MacBook-Air.local>
2026-02-13 14:22:13 +0530

    template

--- template.cpp
+++ template.cpp
@@ -7,12 +7,9 @@
 #include <string>
 #include <algorithm>
 using namespace std;
-#define com ', ' 
 #define sp ' '  
 #define nl '\n'
 #define pb push_back
-#define out cout
-#define in cin
 #define ll long long
 #define mp make_pair
 #define inf int(1e9)
@@ -22,6 +19,18 @@ template <class K, class V> using umap = unordered_map<K, V>;
 template <class T> using vv = vector<vector<T>>;
 template <class T> using pqmax = priority_queue<T>;
 template <class T> using pqmin = priority_queue<T, vector<T>, greater<T>>;
+
+template<typename... Args>
+void out(const Args&... args){
+    int arr[] = {0, (cout<<args, 0)...};
+    (void)arr;
+}
+
+template<typename... Args>
+void in(Args&... args){
+    int arr[] = {0, (cin>>args, 0)...};
+    (void)arr;
+}
 
 
 // **********************************
