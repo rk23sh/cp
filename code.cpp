7f8e59b7d50aef03342153b0575d10c2fb3f8a88
Rounak <rounak@Rounaks-MacBook-Air.local>
2026-02-15 17:40:14 +0530

    done

--- code.cpp
+++ code.cpp
@@ -22,14 +22,14 @@ using namespace std;
 #define rall(x) x.rbegin(), x.rend()
 #define repeat(x) for(int i=0; i<x; i++)
 #define loop_it(x) for(auto it=x.begin(); it != x.end(); it++)
-#define loop(x) for(auto i: x)
-#define grid(n,m) vector<vector<int>> grid(n, vector<int>(m));
-#define umap(K,V) unordered_map<K,V> umap;
-#define uset(T) unordered_set<T> uset;
-#define arr(T,n) vector<T> arr(n);
-template <typename T> using uset = unordered_set<T>;
-template <class K, class V> using umap = unordered_map<K, V>;
-template <class T> using vv = vector<vector<T>>;
+#define loop(x,ptr) for(auto ptr: x)
+#define grid(n,m,T) vector<vector<T>> grid(n, vector<T>(m));
+#define umap(var,K,V) unordered_map<K,V> var;
+#define uset(var,T) unordered_set<T> var;
+#define vec(var,T,sz) vector<T> var(sz);
+// template <typename T> using uset = unordered_set<T>;
+// template <class K, class V> using umap = unordered_map<K, V>;
+// template <class T> using vv = vector<vector<T>>;
 template <class T> using pqmax = priority_queue<T>;
 template <class T> using pqmin = priority_queue<T, vector<T>, greater<T>>;
 
@@ -53,7 +53,7 @@ int main() {
     cin.tie(NULL);
 
     // code:
- \
+    vec(int,0,v);
     
 
 }
